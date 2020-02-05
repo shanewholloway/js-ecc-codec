8133d7f676e7623657cc076ea878d30a00acf8bf
Shane Holloway <shane.holloway@ieee.org>
2020-02-04 18:02:47 -0700

    Working ECDSA api and codecs

--- rollup.config.js
+++ rollup.config.js
@@ -9,9 +9,12 @@ const configs = []
 export default configs
 
 const sourcemap = true
-const external = ['crypto']
+const external_nodejs = ['crypto']
 
 const plugins = [ rpi_resolve({preferBuiltins: true}) ]
+const plugins_generic = [
+  rpi_jsy({defines: {}}),
+  ... plugins ]
 const plugins_nodejs = [
   rpi_jsy({defines: {PLAT_NODEJS: true}}),
   ... plugins ]
@@ -23,37 +26,43 @@ const plugins_min = [
   rpi_terser({}) ]
 
 
-add_jsy('index', pkg_name)
+add_jsy('index', {module_name: pkg_name})
 
 add_jsy('ecc/index')
-add_jsy('ecc/api')
+add_jsy('ecc/api', {skip_generic: true})
 add_jsy('ecc/ecc_encode')
 add_jsy('ecc/ecc_decode')
 add_jsy('ecc/ecc_jwk')
 
 add_jsy('ecdsa/index')
-add_jsy('ecdsa/api')
+add_jsy('ecdsa/api', {skip_generic: true})
 add_jsy('ecdsa/ecdsa_encode')
 add_jsy('ecdsa/ecdsa_decode')
 
-add_jsy('ecdh/api')
+add_jsy('ecdh/api', {skip_generic: true})
 
 
-function add_jsy(src_name, module_name) {
-  if (!module_name) module_name = `${pkg_name}_${src_name}`
+function add_jsy(src_name, opt={}) {
+  let module_name = opt.module_name || `${pkg_name}_${src_name}`
+
+  if (plugins_generic && !opt.skip_generic)
+    configs.push({
+      input: `code/${src_name}.jsy`,
+      plugins: plugins_generic,
+      output: { file: `esm/${src_name}.mjs`, format: 'es', sourcemap }})
 
   if (plugins_nodejs)
     configs.push({
       input: `code/${src_name}.jsy`,
-      plugins: plugins_nodejs, external,
+      plugins: plugins_nodejs, external: external_nodejs,
       output: [
         { file: `cjs/${src_name}.cjs`, format: 'cjs', exports:'named', sourcemap },
-        { file: `esm/${src_name}.mjs`, format: 'es', sourcemap } ]})
+        { file: `esm/node/${src_name}.mjs`, format: 'es', sourcemap } ]})
 
   if (plugins_web)
     configs.push({
       input: `code/${src_name}.jsy`,
-      plugins: plugins_web, external,
+      plugins: plugins_web,
       output: [
         { file: `umd/${src_name}.js`, format: 'umd', name:module_name, exports:'named', sourcemap },
         { file: `esm/web/${src_name}.js`, format: 'es', sourcemap } ]})
@@ -61,6 +70,6 @@ function add_jsy(src_name, module_name) {
   if (plugins_min)
     configs.push({
       input: `code/${src_name}.jsy`,
-      plugins: plugins_min, external,
+      plugins: plugins_min,
       output: { file: `umd/${src_name}.min.js`, format: 'umd', name:module_name, exports:'named', sourcemap }})
 }
--- test/rollup.config.js
+++ test/rollup.config.js
@@ -2,24 +2,24 @@ import rpi_resolve from '@rollup/plugin-node-resolve'
 import rpi_jsy from 'rollup-plugin-jsy-lite'
 
 const sourcemap = 'inline'
+const external_nodejs = ['crypto']
 
-const plugins = [ rpi_resolve() ]
+const browser_globals = {'crypto': 'globalThis.crypto'}
+
 const plugins_nodejs = [
   rpi_jsy({defines: {PLAT_NODEJS: true}}),
-  ... plugins ]
+  rpi_resolve({preferBuiltins: true}), ]
+
 const plugins_web = [
   rpi_jsy({defines: {PLAT_WEB: true}}),
-  ... plugins ]
+  rpi_resolve({preferBuiltins: true}), ]
 
 
 export default [
   { input: `./unittest.jsy`, context: 'window', plugins: plugins_web,
-    output: { file: './__unittest.iife.js', format: 'iife', name: `test_asn1_codec`, sourcemap } },
+    output: { file: './__unittest.iife.js', format: 'iife', name: `test_asn1_codec`, sourcemap, } },
 
-  { input: `./unittest.jsy`, plugins: plugins_nodejs,
+  { input: `./unittest.jsy`, plugins: plugins_nodejs, external: external_nodejs,
     output: { file: './__unittest.cjs.js', format: 'cjs', sourcemap } },
-
-  //{ input: `./unit/ecdh/test.jsy`, plugins: plugins_nodejs,
-  //  output: { file: './oow/ecdh.js', format: 'cjs', sourcemap } },
 ]
 
